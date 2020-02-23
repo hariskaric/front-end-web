cdd54654c8a74a17439ad1033f787aa1d4a21e4e
hariskaric <harris.karic@hotmail.com>
2020-02-23 11:21:16 +0100

    Zadatak [ZAPREMINA VALJKA]

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-var poluprecnik = prompt("UNESITE POLUPREČNIK: ");
-var visina = prompt("UNESITE VISINU VALJKA: ");
+var poluprecnik = parseInt(prompt("UNESITE POLUPREČNIK: "));
+var visina = parseInt(prompt("UNESITE VISINU VALJKA: "));
 var Pi = 3.14;
 var zapremina = parseInt(poluprecnik) * parseInt(poluprecnik) * Pi * parseInt(visina);
 alert("ZAPREMINA VALJKA IZNOSI: " + zapremina);
+
+console.log(typeof poluprecnik);
+console.log(typeof visina);
