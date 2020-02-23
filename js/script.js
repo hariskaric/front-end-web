d6485b2b47fae95eea8df16f319d8bf4e9e282c7
hariskaric <harris.karic@hotmail.com>
2020-02-23 07:24:33 +0100

    URAĐEN NOVI ZADATAK [ZAPREMINA VALJKA]

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var poluprecnik = prompt("UNESITE POLUPREČNIK: ");
+var visina = prompt("UNESITE VISINU VALJKA: ");
+var Pi = 3.14;
+var zapremina = parseInt(poluprecnik) * parseInt(poluprecnik) * Pi * parseInt(visina);
+alert("ZAPREMINA VALJKA IZNOSI: " + zapremina);
