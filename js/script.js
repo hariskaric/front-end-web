928a0bfd8bcb76f755b123690dad514af0911711
hariskaric <harris.karic@hotmail.com>
2020-03-02 13:25:14 +0100

    [DONE] Program za obavljanje osnovnih aritmetičkih operacija

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
-var poluprecnik = parseInt(prompt("UNESITE POLUPREČNIK: "));
-var visina = parseInt(prompt("UNESITE VISINU VALJKA: "));
-var Pi = 3.14;
-var zapremina = (poluprecnik) * (poluprecnik) * Pi * (visina);
-alert("ZAPREMINA VALJKA IZNOSI: " + zapremina);
+let brojJedan = parseInt(prompt("UNESITE PRVI BROJ: "));
+let operacija = prompt("Unesite aritmetičku operaciju: (+, -, *, /)");
+let brojDva = parseInt(prompt("UNESITE DRUGI BROJ: "));
 
-console.log(typeof poluprecnik);
-console.log(typeof visina);
+switch (operacija) {
+  case "+":
+    document.write(
+      `<h1>Brojevi koji su unešeni su ${brojJedan} & ${brojDva}. <br> Aritmetička operacija koja je korištena: <span style=color:red;> + SABIRANJE </span>  <br> ${brojJedan} + ${brojDva} = `,
+      brojJedan + brojDva,
+      `</h1>`
+    );
+    break;
+
+  case "-":
+    document.write(
+      `<h1>Brojevi koji su unešeni su ${brojJedan} & ${brojDva}. <br> Aritmetička operacija koja je korištena: <span style=color:red;> - ODUZIMANJE </span>  <br> ${brojJedan} - ${brojDva} = `,
+      brojJedan - brojDva,
+      `</h1>`
+    );
+    break;
+
+  case "*":
+    document.write(
+      `<h1>Brojevi koji su unešeni su ${brojJedan} & ${brojDva}. <br> Aritmetička operacija koja je korištena: <span style=color:red;> * MNOŽENJE </span>  <br> ${brojJedan} * ${brojDva} = `,
+      brojJedan * brojDva,
+      `</h1>`
+    );
+    break;
+
+  case "/":
+    document.write(
+      `<h1>Brojevi koji su unešeni su ${brojJedan} & ${brojDva}. <br> Aritmetička  operacija koja je korištena: <span style=color:red;> / DIJELJENJE </span>  <br> ${brojJedan} / ${brojDva} = `,
+      brojJedan / brojDva,
+      `</h1>`
+    );
+    break;
+
+  default:
+    document.write(`<h1>Unijeli ste pogrešanu aritmetičku operaciju, pokušajte ponovo! Ponuđene opcije su (+ SABIRANJE, - ODUZIMANJE, * MNOŽENJE, / DIJELJENJE) </h1>`);
+}
